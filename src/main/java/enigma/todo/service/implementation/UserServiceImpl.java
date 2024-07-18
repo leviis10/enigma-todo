18ced6e73482aef63a404e707d8d5da64ed5a18b
Levi <alevindrajaya@gmail.com>
2024-07-18 23:00:11 +0700

    super minor fix

--- src/main/java/enigma/todo/service/implementation/UserServiceImpl.java
+++ src/main/java/enigma/todo/service/implementation/UserServiceImpl.java
@@ -10,14 +10,11 @@ import enigma.todo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
